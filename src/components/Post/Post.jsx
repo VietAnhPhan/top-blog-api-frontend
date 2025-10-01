307ec3e635b59b34201478ed8b1a4cee3f0d3b02
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-01 17:02:24 +0700

    Implement comment section and submit comment function

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,19 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context";
 import { useParams } from "react-router";
+import Comment from "../Comment/Component";
 
 const Post = () => {
   const authContext = useContext(AuthContext);
   const params = useParams();
+  const [enableSubmit, setEnablesubmit] = useState(false);
+  const [comment, setComment] = useState("");
   const [title, setTitle] = useState("");
   const [body, setBody] = useState(
     "<p>This is the initial content of the editor.</p>"
   );
+
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     async function getPost() {
@@ -30,7 +35,26 @@ const Post = () => {
       }
     }
 
+    async function getComments() {
+      const response = await fetch(
+        `http://localhost:3000/comments?postId=${params.postId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `bearer ${authContext.token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        const result = await response.json();
+        setComments(result.comments);
+      }
+    }
+
     getPost();
+    getComments();
   });
 
   async function submitComment(formData) {
@@ -62,15 +86,33 @@ const Post = () => {
               name="comment"
               type="text"
               placeholder="Leave your comment here..."
+              value={comment}
+              onChange={(e) => {
+                setComment(e.target.value);
+                if (!enableSubmit) setEnablesubmit(true);
+              }}
             ></textarea>
           </p>
-
-          <div className="flex gap-2.5">
+          {enableSubmit ? (
             <button type="submit" className="button-solid">
               Submit
             </button>
-          </div>
+          ) : (
+            <button disabled type="submit" className="button-solid bg-gray-500">
+              Submit
+            </button>
+          )}
         </form>
+        {comments.length > 0 && (
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>
+                <p>{comment.user.username}</p>
+                <p>{comment.comment}</p>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
