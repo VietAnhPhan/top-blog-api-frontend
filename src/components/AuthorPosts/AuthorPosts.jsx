2785be89614f49bf74e0d74eb19f3d8c6b02e5f3
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-02 10:12:24 +0700

    Implement Comment component and inject to AuthorPost component - show all comments from readers for posts of particular author - add edit comment function

--- src/components/AuthorPosts/AuthorPosts.jsx
+++ src/components/AuthorPosts/AuthorPosts.jsx
@@ -1,10 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context";
 import AuthorPost from "../AuthorPost/AuthorPost";
+import Comment from "../Comment/Component";
 
 const AuthorPosts = () => {
   const authContext = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     async function getPosts() {
@@ -25,7 +27,25 @@ const AuthorPosts = () => {
       setPosts(authorPosts);
     }
 
+    async function getComments() {
+      const response = await fetch(
+        `http://localhost:3000/comments?authorId=${authContext.userId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `bearer ${authContext.token}`,
+          },
+        }
+      );
+
+      const result = await response.json();
+
+      setComments(result.comments);
+    }
+
     getPosts();
+    getComments();
   }, [authContext.userId, authContext.token]);
   return (
     <>
@@ -43,6 +63,22 @@ const AuthorPosts = () => {
               })}
             </ul>
           </div>
+        )}
+
+        <h2 className="text-center mt-16">Comments from readers</h2>
+        {comments.length > 0 && (
+          <ul>
+            {comments.map((comment, index) => {
+              return (
+                <li className="pt-5" key={index}>
+                  <Comment
+                    comment={comment}
+                    authContext={authContext}
+                  ></Comment>
+                </li>
+              );
+            })}
+          </ul>
         )}
       </div>
     </>
--- src/components/Comment/Component.jsx
+++ src/components/Comment/Component.jsx
@@ -1,9 +1,48 @@
-const Comment = ({ user, comment }) => {
+import { useState } from "react";
+
+const Comment = (props) => {
+  const [comment, setComment] = useState(props.comment);
+
+  async function handleSubmit(commentId) {
+    const response = await fetch(
+      `http://localhost:3000/comments/${commentId}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({ comment: comment.comment }),
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `bearer ${props.authContext.token}`,
+        },
+      }
+    );
+
+    if (response.ok) {
+      const result = await response.json();
+      setComment({ ...comment, comment: result.comment });
+    }
+    return;
+  }
   return (
-    <div>
-      <span>{user}</span>
-      <span>{comment}</span>
-    </div>
+    <>
+      <p>Title: {comment.post.title}</p>
+      <p>{comment.user.username}</p>
+      <p>at {comment.created_at}</p>
+      <textarea
+        value={comment.comment}
+        className="rounded-md"
+        cols={100}
+        onChange={(e) => setComment({ ...comment, comment: e.target.value })}
+      ></textarea>
+      <button
+        type="button"
+        className="button-solid"
+        onClick={() => {
+          handleSubmit(comment.id);
+        }}
+      >
+        Submit
+      </button>
+    </>
   );
 };
 
